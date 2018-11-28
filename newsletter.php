2ffb433263227c703ea23dd8ca3b30dd8f518b59
Tomas Krejci <tomas.krejci@superkoderi.cz>
2018-11-28 10:00:05 +0100

    Drobné ladění hlavičky

--- newsletter.php
+++ newsletter.php
@@ -203,13 +203,13 @@
 							<td class="sk-block">
 								<table class="sk-block-table" width="100%" border="0" cellspacing="0" cellpadding="0" style="margin:0; padding:0; font-size:12px; font-family: Arial, Helvetica, sans-serif; width:100%; text-align: left;">
 									<tr class="sk-block">
-										<td class="sk-block sk-pl20 sk-pr20 sk-pb0" style="vertical-align: top; padding: 15px 0 50px;">
-											<table width="100%" border="0" cellspacing="0" cellpadding="0" style="margin:0; padding:0; font-size:12px; font-family: Arial, Helvetica, sans-serif; width:100%; text-align: left;">
+										<td class="sk-block sk-pl20 sk-pr20 sk-pb0" style="vertical-align: top; padding: 15px 20px 50px 0;">
+											<!-- <table width="100%" border="0" cellspacing="0" cellpadding="0" style="margin:0; padding:0; font-size:12px; font-family: Arial, Helvetica, sans-serif; width:100%; text-align: left;">
 												<tr>
 													<td style="width: 120px;">
 														<p style="margin: 0; line-height: 1;">
 															<a href="#" style="text-decoration: none;">
-																<img src="http://newsletter-muni2018.superkoderi.cz/img/logo-muni.png" alt="Masarykova univerzita" width="100" height="29" style="vertical-align: top; border: none;">
+																<img src="./img/logo-muni.png" alt="Masarykova univerzita" width="100" height="29" style="vertical-align: top; border: none;">
 															</a>
 														</p>
 													</td>
@@ -219,7 +219,12 @@
 														</p>
 													</td>
 												</tr>
-											</table>
+											</table> -->
+											<p style="margin: 3px 0 0; line-height: 1;">
+												<a href="#" style="text-decoration: none;">
+													<img src="./img/logo-muni-sci.png" alt="MUNI - SCI" width="310" height="72" style="vertical-align: top; border: none;">
+												</a>
+											</p>
 										</td>
 										<td class="sk-block sk-pl20 sk-pr20 sk-pb20 sk-tal" style="vertical-align: top; text-align: right; color: #0000DC; padding: 15px 0 50px;">
 											<p style="margin: 0; line-height: 18px;">
@@ -227,8 +232,13 @@
 													<a href="#" style="color: #0000DC;">Webová verze</a> &nbsp;|&nbsp;
 													<a href="#" style="color: #0000DC;">online.muni.cz</a> &nbsp;|&nbsp;
 												</span>
-												Newsletter 02/2019 &nbsp;|&nbsp;
-												<span class="sk-hide">Odesláno</span> 17. 2. 2019<span class="sk-hide">, 8:36</span>
+												<span style="white-space: nowrap;">
+													Newsletter 02/2019
+												</span>
+												<span style="white-space: nowrap;">
+													&nbsp;|&nbsp;
+													<span class="sk-hide">Odesláno</span> 17. 2. 2019<span class="sk-hide">, 8:36</span>
+												</span>
 											</p>
 										</td>
 									</tr>
